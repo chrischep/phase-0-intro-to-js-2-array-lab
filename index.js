834481e7d6a5b0205433c927f43c75146c2b2537
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-21 21:11:40 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats =["Milo","Otis","Garfield"]
+function destructivelyAppendCat (name){
+    return cats.push(name);
+    }
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function appendCat(name){
+    let newCats=cats.slice()
+    newCats.push(name)
+    return newCats;
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function prependCat(name){
+    let pCats=cats.slice()
+    pCats.unshift(name)
+    return pCats;
+}
+function removeLastCat(){
+    let q=cats.slice()
+    q.pop()
+    return q
+}
+function removeFirstCat(){
+    let b =cats.slice()
+    b.shift()
+    return b
+}
